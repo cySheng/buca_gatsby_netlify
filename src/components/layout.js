467e3fca3b9a6a441e6afe5921d606b04e4e1afd
Sheng <cyusheng93@gmail.com>
2020-07-17 22:16:26 +0800

    Tweaked front page design.

--- src/components/layout.js
+++ src/components/layout.js
@@ -32,6 +32,13 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
+      <section class="svg-wrapper">
+        <section class="custom-shape-divider-top-1594991478">
+          <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+          </svg>
+        </section>
+      </section>
       <div
         // style={{
         //   margin: `0 auto`,

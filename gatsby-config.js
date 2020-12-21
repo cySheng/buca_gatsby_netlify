6a02d314a58a4fca6860347ea3dd756fc1fa47da
Sheng <cyusheng93@gmail.com>
2020-12-21 11:56:50 +0800

    fixed broken plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -39,22 +39,32 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-prefetch-google-fonts`,
+      resolve: `gatsby-plugin-google-fonts`,
       options: {
         fonts: [
-          {
-            family: `Poppins`,
-            variants: [`400`, `700`]
-          },
+          `poppins\:400,700`,
+          `source sans pro\:300,400,400i,700` // you can also specify font weights and styles
         ],
-      },
-    },
-    {
-      resolve: 'gatsby-plugin-robots-txt',
-      options: {
-        policy: [{ userAgent: '*', allow: '/' }]
+        display: 'swap'
       }
     },
+    // {
+    //   resolve: `gatsby-plugin-prefetch-google-fonts`,
+    //   options: {
+    //     fonts: [
+    //       {
+    //         family: `Poppins`,
+    //         variants: [`400`, `700`]
+    //       },
+    //     ],
+    //   },
+    // },
+    // {
+    //   resolve: 'gatsby-plugin-robots-txt',
+    //   options: {
+    //     policy: [{ userAgent: '*', allow: '/' }]
+    //   }
+    // },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-catch-links`,
